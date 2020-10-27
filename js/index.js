6e31b2686fb62f8ef8b46abf0a3291e1f28a30f4
Gabriel Bustos Web <gabrielbustos01@gmail.com>
2020-10-27 17:19:39 -0300

    pagina de prueba

---
+++ js/index.js
@@ -0,0 +1,24 @@
+jQuery(document).ready( $ =>  {
+    $('.menu-principal').slicknav({
+        label: ''
+    });
+
+});
+
+
+window.onscroll = () => {
+    const scroll = window.scrollY;
+
+    
+    const headerNav = document.querySelector('.barra-navegacion');
+    const documentBody = document.querySelector('header');
+
+    // si la cantidad de escroll es mayor a, agregar una clase
+    if(scroll > 300) {
+        headerNav.classList.add('fixed-top');
+        documentBody.classList.add('ft-activo');
+    } else {
+        documentBody.classList.remove('ft-activo');
+        headerNav.classList.remove('fixed-top');
+    }
+}
