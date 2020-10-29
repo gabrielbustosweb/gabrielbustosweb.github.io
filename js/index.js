3859d296c40569e63f2527492698a08843375b7a
Gabriel Bustos Web <gabrielbustos01@gmail.com>
2020-10-29 18:13:54 -0300

    slickNav y navegacion

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
 jQuery(document).ready( $ =>  {
     $('.menu-principal').slicknav({
-        label: ''
+        label: '',
+        closeOnClick:true
+    });
+    $("a").on('click', function(event){
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
     });
 
 });
@@ -11,14 +24,17 @@ window.onscroll = () => {
 
     
     const headerNav = document.querySelector('.barra-navegacion');
-    const documentBody = document.querySelector('header');
+    const slickNav = document.querySelector('.slicknav_menu');
+    const documentBody = document.querySelector('body');
 
     // si la cantidad de escroll es mayor a, agregar una clase
     if(scroll > 300) {
         headerNav.classList.add('fixed-top');
+        slickNav.classList.add('fixed-top');
         documentBody.classList.add('ft-activo');
     } else {
         documentBody.classList.remove('ft-activo');
+        slickNav.classList.remove('fixed-top');
         headerNav.classList.remove('fixed-top');
     }
 }
