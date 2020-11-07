1c02a831073c7cbb8497103607d88d90ded38fe3
Gabriel Bustos Web <gabrielbustos01@gmail.com>
2020-11-07 12:09:40 -0300

    services

--- js/index.js
+++ js/index.js
@@ -38,4 +38,10 @@ window.onscroll = () => {
         headerNav.classList.remove('fixed-top');
     }
 };
-
+$(".toTop").click(function () {
+   //1 second of animation time
+   //html works for FFX but not Chrome
+   //body works for Chrome but not FFX
+   //This strange selector seems to work universally
+   $("html, body").animate({scrollTop: 0}, 1000);
+});
